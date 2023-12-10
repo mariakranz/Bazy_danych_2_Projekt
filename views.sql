c1ea40ca5707530c1a7080f82d4961ab705932eb
Suryss <61971720+Suryss@users.noreply.github.com>
2023-12-10 11:33:53 +0100

    views

---
+++ views.sql
@@ -0,0 +1,42 @@
+#widok budynku
+CREATE VIEW BuildingInfo AS
+SELECT 
+    b.id AS BuildingID,
+    b.City,
+    b.Street,
+    d.Description,
+    COUNT(r.Number) AS RoomCount
+FROM buildings b
+JOIN descriptions d ON b.DescriptionID = d.id
+LEFT JOIN rooms r ON b.id = r.BuildingID
+GROUP BY b.id;
+#widok pokoju
+CREATE VIEW RoomInfo AS
+SELECT
+        r.id, 
+        r.Number, 
+        r.Type, 
+        r.BedsNumber, 
+        d.Description as `RoomDescription`, 
+        bi.City, 
+        bi.Street, 
+        bi.Description as `BuildingDescription`,
+        bi.RoomCount
+FROM rooms r
+JOIN descriptions d ON r.DescriptionID = d.ID
+JOIN BuildingInfo bi ON r.BuildingID = bi.BuildingID;
+# widok pracownika
+CREATE VIEW EmployeeInfo AS
+SELECT 
+    e.id AS EmployeeID,
+    e.name AS EmployeeName,
+    e.surname AS EmployeeSurname,
+    e.phone AS EmployeePhone,
+    e.email AS EmployeeEmail,
+    e.privilege AS EmployeePrivilege,
+    d.Name AS DepartmentName
+FROM 
+    employees e
+JOIN 
+    departments d ON e.DepartmentID = d.id;
+
